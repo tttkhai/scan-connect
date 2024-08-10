ba55b2bd6dce84030c2f125961b85e257cc63934
tttkhai <tttkhai93@gmail.com>
2024-08-10 14:51:55 -0500

    fixed env vars issues

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,16 +1,20 @@
 import { registerAs } from '@nestjs/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const config = {
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.POSTGRES_PORT) || 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'scan-connect',
-  entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [`${__dirname}/../src/**/*.entity{.ts,.js}`],
   synchronize: false,
-  migrations: [`${__dirname}/../../db/migrations/*{.ts,.js}`],
+  migrations: [`${__dirname}/migration/*{.ts,.js}`],
   migrationsTableName: 'migrations',
 };
 
